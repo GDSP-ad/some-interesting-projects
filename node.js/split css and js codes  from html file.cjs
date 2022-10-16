3d55117c72861f04175067d1e79555758a88fde0
GDSP-ad <84957199+GDSP-ad@users.noreply.github.com>
2022-10-16 17:10:47 +0800

    comment
    从相同路径下的index.html文件中分离出js和css内容，并写入相同路径下的index.js和index.css文件中

--- node.js/split css and js codes  from html file.cjs
+++ node.js/split css and js codes  from html file.cjs
@@ -7,6 +7,7 @@ fs.readFile(path.join(__dirname, './index.html'), 'utf8', (err, data) => {
     if (!err) {
         cssContent = data.match(/(?<=<style>).*(?=<\/style>)/s)[0]; // 取出css内容
         jsContent = data.match(/(?<=<script>).*(?=<\/script>)/s)[0]; // 取出js内容
+        
         fs.writeFile(path.join(__dirname,'./index.css'),cssContent,err=>{
             if(!err){
                 console.log('css file has been split');
