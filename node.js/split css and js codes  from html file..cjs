63fe42dac5c51575ab28e14c33a9356ec815d382
GDSP-ad <84957199+GDSP-ad@users.noreply.github.com>
2022-10-16 17:02:36 +0800

    Create split css and js codes  from html file..cjs
    从相同路径下的index.html文件中分离出js和css内容，并写入相同路径下的index.js和index.css文件中

---
+++ node.js/split css and js codes  from html file..cjs
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+var cssContent, jsContent;
+
+fs.readFile(path.join(__dirname, './index.html'), 'utf8', (err, data) => {
+    if (!err) {
+        cssContent = data.match(/(?<=<style>).*(?=<\/style>)/s)[0]; // 取出css内容
+        jsContent = data.match(/(?<=<script>).*(?=<\/script>)/s)[0]; // 取出js内容
+        fs.writeFile(path.join(__dirname,'./index.css'),cssContent,err=>{
+            if(!err){
+                console.log('css file has been split');
+            }
+        });
+        fs.writeFile(path.join(__dirname,'./index.js'),jsContent,err=>{
+            if(!err){
+                console.log('js file has been split');
+            }
+        });
+    }
+})
